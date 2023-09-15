6981cc71ebf8b4ef736d46c945d4582407cc5301
tzxyz <tz.zhuo@protonmail.com>
2023-09-15 20:34:35 +0800

    first commit

---
+++ main.ts
@@ -0,0 +1,34 @@
+import { ApiPromise, Keyring } from '@polkadot/api';
+import { WsProvider } from '@polkadot/rpc-provider';
+
+async function subscribeTemplateUpdates() {
+  const wsProvider = new WsProvider('ws://127.0.0.1:9944');
+  const api = await ApiPromise.create({ provider: wsProvider });
+
+  const keyring = new Keyring({ type: 'sr25519' });
+
+  // 替换为你的账户地址和密码
+  const account = keyring.addFromUri('//Alice', { name: 'Alice' });
+  const accountId = account.address;
+
+  // 订阅 Template 模块中的值更新
+  api.query.templateModule.something((result: string) => {
+    console.log(`Value updated: ${result.toString()}`);
+  });
+
+  // 订阅 Template 模块中的 SomethingStored 事件
+  api.query.system.events((events: any) => {
+    events.forEach((record: any) => {
+      const { event, phase } = record;
+
+      if (event.section === 'templateModule' && event.method === 'SomethingStored') {
+        const [newValue] = event.data;
+        console.log(`SomethingStored event received with new value: ${newValue}`);
+      }
+    });
+  });
+
+  console.log(`Subscribed to Template updates and events for account: ${accountId}`);
+}
+
+subscribeTemplateUpdates().catch(console.error);
